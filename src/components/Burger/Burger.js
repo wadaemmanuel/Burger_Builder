961bb1265c5a710c59906baa9b26ad4bdcf48ebd
Emmanuel Wada <emmanuelwada@Emmanuels-MBP.attlocal.net>
2019-09-30 15:07:36 -0500

    new burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,11 +5,16 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+    .map( igkey => {
+        return[...Array( props.ingredients[igkey])].map((_,i) => {
+            return <BurgerIngredient key={igKey + i} type = {igKey}/>;
+        });
+    });
     return(
         <div className = {classes.Burger}>
             <BurgerIngredient type = 'bread-top'/>
-            <BurgerIngredient type = 'cheese'/>
-            <BurgerIngredient type = 'meat'/>
+           {transformedIngredients}
             <BurgerIngredient type = 'bread-bottom'/>
         </div>
     );
